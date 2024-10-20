58cb648c12d580722a5ba2c02a7729065e5a1970
Shiv Shankar <ps431352@gmail.com>
2024-10-20 12:22:54 +0530

    resolve cors error

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import weatherRoutes from './routes/weatherRoutes.js';
 import alertRoutes from './routes/alertRoutes.js'
 import { connectDB } from './config/db.js';
@@ -8,6 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 connectDB();
 
